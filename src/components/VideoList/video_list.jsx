b69314442b969f83a5748f7764e8887a2054a95f
vampiree007 <vikrantgupta.psit@gmail.com>
2020-06-13 01:36:07 -0700

    first commit

---
+++ src/components/VideoList/video_list.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import VideoListItem from './video_list_item';
+import './video_list.css';
+
+const VideoList = props => {
+  // Mapping all the videos to create each video item
+  console.log(props)
+  let videos = props.videos.map(video => <VideoListItem {...video} key={video.etag} onVideoSelect={props.onVideoSelect} />);
+  return (
+    <ul className="video-list-wrapper">
+      {videos}
+    </ul>
+  );
+};
+
+export default VideoList;
---
+++ src/key.js
@@ -0,0 +1,3 @@
+const API_KEY = 'AIzaSyDCu0qLGxPlOcJfTa5BP0TPZdKAL2Pwi5M'; //provide your own youtube api
+
+module.exports = API_KEY; 
